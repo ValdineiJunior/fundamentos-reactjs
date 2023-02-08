9f87e3e09844f0ea73af624bc722dd3e3c9a5042
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2023-02-08 07:42:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./assets/Components/Header"
+import { Post } from "./Post"
+
+import './styles.css'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
